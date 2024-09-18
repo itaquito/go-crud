e867b2904153f20e8b06a30c40ce5faba6c6a62d
itaquito <ichilaquiles@gmail.com>
2024-09-17 22:38:24 -0600

    feat: Adds the Movie and Director struct

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// A struct that defines a Movie
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+// A struct that defines a Director
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// An array of Movies
+var movies []Movie
+
+func main() {
+	// Creates the MUX router
+	r := mux.NewRouter()
+
+	// Create some default movies
+	movies = append(movies, Movie{ID: "1", Isbn: "12345", Title: "Movie 1", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "67890", Title: "Movie 2", Director: &Director{Firstname: "Peter", Lastname: "Smith"}})
+
+	fmt.Printf("Starting server at port 8000.\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
